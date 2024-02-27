20e157ce60dc4ff42fe4c0a4f895796c35ede52b
Sherrera90 <steph.herrera90@gmail.com>
2024-02-27 11:14:59 -0500

    testing components

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [currentCountry, setCurrentCountry] = useState('');
+  // const [currentCountry, setCurrentCountry] = useState('');
   const [countries, setCountries] = useState([]);
   const [states, setStates] = useState([]);
-
+ // Store the selected country in a new state
+ const [selectedCountryCode, setSelectedCountryCode ] = useState('');
   useEffect(() => {
     axios
       .get('https://xc-countries-api.fly.dev/api/countries/')
@@ -16,36 +17,42 @@ function App() {
   }, []);
 
   useEffect(() => {
+    if(selectedCountryCode===''){
     axios
       .get(
-        'https://xc-countries-api.fly.dev/api/countries/<country_code>/states/'
+        'https://xc-countries-api.fly.dev/api/countries/${selectedCountryCode}/states/'
       )
       .then((response) => {
         setStates(response.data);
       });
-  }, []);
+  }}, [selectedCountryCode]);
 
   const handleChange = (event: any) => {
     console.log('stephanie', event, event.target.value);
-    setCurrentCountry(event.target.value);
+    setSelectedCountryCode(event.target.value);
   };
-  const changeState = (event) => {
-    console.log('stephanie', event, event.target.value);
-    setStates(event.target.value);
-  };
+  // const changeState = (event:any) => {
+  //   console.log('stephanie', event, event.target.value);
+  //   setStates(event.target.value);
+  // };
   return (
     <div>
       <label>
         <h1> Where Do You Live? </h1>
       </label>
-      <select onChange={handleChange}>
+      <select 
+        name = "countries"
+        id = "countries"
+        // placeholder ="Select Country"
+        onChange={handleChange}>
         {countries.map((country: any) => {
           return <option>{country.name}</option>;
         })}
       </select>
 
       <div>
-        <p> I live in {currentCountry} </p>
+        <p> I live in {states} </p>
+        {/* <p> {currentCountry}</p> */}
       </div>
     </div>
   );
