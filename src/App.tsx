15ced170ef7816dac88418256738783ac12244c1
Sherrera90 <steph.herrera90@gmail.com>
2024-03-01 11:59:34 -0500

    abcmess

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,60 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  // const [currentCountry, setCurrentCountry] = useState('');
+  const [currentCountry, setCurrentCountry] = useState('');
   const [countries, setCountries] = useState([]);
   const [states, setStates] = useState([]);
- // Store the selected country in a new state
- const [selectedState, setSelectedState ] = useState('');
+  // // Store the selected country in a new state
+  // const [stateName, setStateName ] = useState('');
+
   useEffect(() => {
     axios
       .get('https://xc-countries-api.fly.dev/api/countries/')
-      .then((response) => {
+      .then((response: any) => {
+        response.data.sort((a:any,b:any) => {
+          return a.name > b.name ? 1 : -1
+        })
         setCountries(response.data);
       });
   }, []);
 
   useEffect(() => {
-    if(selectedState===''){
-    axios.get(
-        'https://xc-countries-api.fly.dev/api/countries/${selectedState}/states/'
-      )
-      .then((response) => {
+    if (currentCountry === '') {
+      setStates([]);
+      return;
+    }
+    axios.get(`https://xc-countries-api.fly.dev/api/countries/${currentCountry}/states/`)
+      .then((response: any) => {
+        response.data.sort((a:any,b:any) => {
+          return a.name > b.name ? 1 : -1
+        })
         setStates(response.data);
       });
-  }}, [selectedState]);
+  }, [currentCountry]);
+
 
   const handleChange = (event: any) => {
     console.log('stephanie', event, event.target.value);
-    setSelectedState(event.target.value);
+    setCurrentCountry(event.target.value);
   };
-  // const changeState = (event:any) => {
-  //   console.log('stephanie', event, event.target.value);
-  //   setStates(event.target.value);
-  // };
+
+
   return (
     <div>
       <label>
         <h1> Where Do You Live? </h1>
       </label>
-      <select 
-        name = "countries"
-        id = "countries"
-        // placeholder ="Select Country"
+      <select
         onChange={handleChange}>
         {countries.map((country: any) => {
-          return <option>{country.name}</option>;
+          return <option value={country.code}>{country.name}</option>;
         })}
       </select>
-
-      <div>
-        <p> I live in {states} </p>
-        {/* <p> {currentCountry}</p> */}
-      </div>
+      <select>
+        {states.map((state: any) => {
+          return <option value={state.code}>{state.name}</option>;
+        })}
+      </select>
     </div>
   );
 }
